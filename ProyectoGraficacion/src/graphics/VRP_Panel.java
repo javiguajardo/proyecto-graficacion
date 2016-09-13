41c75e4782ebb52b42781b64cd10bdafd70b18e8
guajardo <a282057@uach.mx>
2016-09-13 17:51:29 -0600

    Se cambia paquete

--- ProyectoGraficacion/src/graphics/VRP_Panel.java
+++ ProyectoGraficacion/src/graphics/VRP_Panel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package vrp_visualizer3;
+package graphics;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
